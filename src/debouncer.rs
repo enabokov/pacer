2ed7afc82432fc215be33b530e48334d41000e34
Eduard Nabokov <edwardnabokov@gmail.com>
2020-03-11 19:33:45 +0200

    Implement debouncer (#2)
    * Implement debouncer

---
+++ src/debouncer.rs
@@ -0,0 +1,96 @@
+//! This is documentation for the `debouncer` module
+//!
+//! # Examples
+//!
+//! ```
+//! use pacer::debouncer::{StatusCode, DebounceOptions, Debouncer};
+//!
+//! fn main() {
+//!     let mut debouncer = Debouncer::new(&DebounceOptions {
+//!         period_seconds: 5,
+//!     });
+//!
+//!     request(&mut debouncer);
+//! }
+//!
+//! fn request(debouncer: &mut Debouncer) {
+//!     let status_code: StatusCode = debouncer.debounce();
+//!     match status_code {
+//!         StatusCode::Ok => { println!("Request can be operated") },
+//!         StatusCode::Error => { println!("Something went wrong!") },
+//!         StatusCode::Debounced => { println!("It's debounced!") },
+//!     };
+//! }
+//! ```
+
+use std::time::SystemTime;
+
+/// The `DebounceOptions` struct.
+pub struct DebounceOptions {
+    /// how long to wait
+    /// before proceed with request
+    pub period_seconds: u64,
+}
+
+/// The `StatusCode` enum.
+///
+/// Used to return status of the debounce function.
+#[derive(PartialEq, Debug)]
+pub enum StatusCode {
+    /// Calls can be performed
+    Ok,
+    /// Calls are debounced
+    /// Required to stop requesting and wait some time
+    Debounced,
+    /// Debounce went wrong.
+    /// Something does not work properly.
+    Error,
+}
+
+/// The `Debouncer` struct.
+///
+/// The main struct that is used to enable debouncing.
+/// It implements 2 methods:
+/// - new
+/// - debounce
+///
+pub struct Debouncer<'a> {
+    /// Each Debouncer has options to set.
+    options: &'a DebounceOptions,
+    /// Since the time a last call was debounced
+    _last_request: SystemTime,
+}
+
+impl<'a> Debouncer<'a> {
+    /// Create an instance of the Debouncer
+    /// with its configured options
+    /// and set initial values
+    pub fn new(options: &'a DebounceOptions) -> Debouncer {
+        Debouncer {
+            options: options,
+            _last_request: SystemTime::now(),
+        }
+    }
+
+    /// Call `debounce` to limit traffic.
+    /// e.g it can be called within HTTP requests.
+    pub fn debounce(&mut self) -> StatusCode {
+        let past_time_in_secs: u64;
+        {
+            let current_time = SystemTime::now();
+            past_time_in_secs = current_time
+                .duration_since(self._last_request)
+                .unwrap()
+                .as_secs();
+        }
+
+        if past_time_in_secs < self.options.period_seconds {
+            self._last_request = SystemTime::now();
+            println!("Debounced!");
+            return StatusCode::Debounced;
+        }
+
+        self._last_request = SystemTime::now();
+        return StatusCode::Ok;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+pub mod debouncer;
 pub mod throttler;
 
 #[cfg(test)]
 mod tests {
-    use super::throttler::{StatusCode, ThrottleOptions, Throttler};
+    use super::debouncer::{DebounceOptions, Debouncer};
+    use super::throttler::{ThrottleOptions, Throttler};
 
     #[test]
     fn check_throttle_options_creation() {
@@ -16,24 +18,50 @@ mod tests {
     }
 
     #[test]
+    fn check_debounce_options_creation() {
+        //! create Debounce which refuses all requests within 5 seconds
+        let options = DebounceOptions { period_seconds: 5 };
+        assert_eq!(options.period_seconds, 5);
+    }
+
+    #[test]
     fn check_throttle_work() {
         let mut throttler = Throttler::new(&ThrottleOptions {
             max_call_count: 2,
             period_seconds: 5,
         });
 
-        let mut status_code: StatusCode = throttler.throttle();
-        assert_eq!(status_code, StatusCode::Ok);
+        let mut status_code: super::throttler::StatusCode = throttler.throttle();
+        assert_eq!(status_code, super::throttler::StatusCode::Ok);
 
         status_code = throttler.throttle();
-        assert_eq!(status_code, StatusCode::Ok);
+        assert_eq!(status_code, super::throttler::StatusCode::Ok);
 
         status_code = throttler.throttle();
-        assert_eq!(status_code, StatusCode::Throttled);
+        assert_eq!(status_code, super::throttler::StatusCode::Throttled);
 
         std::thread::sleep(std::time::Duration::from_secs(5));
 
         status_code = throttler.throttle();
-        assert_eq!(status_code, StatusCode::Ok);
+        assert_eq!(status_code, super::throttler::StatusCode::Ok);
+    }
+
+    #[test]
+    fn check_debounce_work() {
+        let mut debouncer = Debouncer::new(&DebounceOptions { period_seconds: 5 });
+
+        let mut status_code: super::debouncer::StatusCode = debouncer.debounce();
+        assert_eq!(status_code, super::debouncer::StatusCode::Debounced);
+
+        status_code = debouncer.debounce();
+        assert_eq!(status_code, super::debouncer::StatusCode::Debounced);
+
+        status_code = debouncer.debounce();
+        assert_eq!(status_code, super::debouncer::StatusCode::Debounced);
+
+        std::thread::sleep(std::time::Duration::from_secs(5));
+
+        status_code = debouncer.debounce();
+        assert_eq!(status_code, super::debouncer::StatusCode::Ok);
     }
 }
